08fc7984bd46488794bd5251539d1a9726fe8b84
Marcia Rasi <marcia.pedals@gmail.com>
2026-02-07 09:33:02 -0800

    add precommit linters

--- flake.nix
+++ flake.nix
@@ -3,18 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, git-hooks }:
     let
       system = "aarch64-darwin";
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
+      pre-commit = git-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          ruff-format = {
+            enable = true;
+          };
+          ruff = {
+            enable = true;
+          };
+          shellcheck = {
+            enable = true;
+          };
+        };
+      };
     in
     {
+      checks.${system}.pre-commit = pre-commit;
+
       devShells.${system}.default = pkgs.mkShell {
+        inherit (pre-commit) shellHook;
         packages = [
           pkgs.tart
           pkgs.softnet
